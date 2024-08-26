79977b9d44381bc60dae6faaf849168d264ff6ef
Mahesh-Karanam <129201876+Mahesh-Karanam@users.noreply.github.com>
2024-08-26 12:04:48 +0530

    Add files via upload

---
+++ TypingSpeedTest/TypingSpeedTest/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace TypingSpeedTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelTestText = new System.Windows.Forms.Label();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.labelTimer = new System.Windows.Forms.Label();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.labelResults = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelTestText
+            // 
+            this.labelTestText.AutoSize = true;
+            this.labelTestText.Location = new System.Drawing.Point(12, 9);
+            this.labelTestText.Name = "labelTestText";
+            this.labelTestText.Size = new System.Drawing.Size(0, 13);
+            this.labelTestText.TabIndex = 0;
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Enabled = false;
+            this.textBoxInput.Location = new System.Drawing.Point(12, 48);
+            this.textBoxInput.Multiline = true;
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(276, 109);
+            this.textBoxInput.TabIndex = 1;
+            // 
+            // labelTimer
+            // 
+            this.labelTimer.AutoSize = true;
+            this.labelTimer.Location = new System.Drawing.Point(21, 160);
+            this.labelTimer.Name = "labelTimer";
+            this.labelTimer.Size = new System.Drawing.Size(24, 13);
+            this.labelTimer.TabIndex = 2;
+            this.labelTimer.Text = "60s";
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(100, 191);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 25);
+            this.buttonStart.TabIndex = 3;
+            this.buttonStart.Text = "start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            // 
+            // labelResults
+            // 
+            this.labelResults.AutoSize = true;
+            this.labelResults.Location = new System.Drawing.Point(15, 160);
+            this.labelResults.Name = "labelResults";
+            this.labelResults.Size = new System.Drawing.Size(0, 13);
+            this.labelResults.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.labelResults);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.labelTimer);
+            this.Controls.Add(this.textBoxInput);
+            this.Controls.Add(this.labelTestText);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelTestText;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.Label labelTimer;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Label labelResults;
+    }
+}
+
---
+++ TypingSpeedTest/TypingSpeedTest/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace TypingSpeedTest
+{
+    public partial class Form1 : Form
+    {
+        private List<string> testTexts;
+        private string currentTestText;
+        private Stopwatch stopwatch;
+        private Timer timer;
+        private int timeLimit = 60; // seconds
+        private Random random;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeTest();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // This method is intentionally left empty
+            // It gets called when the form is loaded
+        }
+
+        private void InitializeTest()
+        {
+            testTexts = new List<string>
+            {
+                "A Fox one day spied a beautiful bunch of ripe grapes hanging from a vine trained along the branches of a tree. The grapes seemed ready to burst with juice, and the Fox's mouth watered as he gazed longingly at them.The bunch hung from a high branch, and the Fox had to jump for it. The first time he jumped he missed it by a long way. So he walked off a short distance and took a running leap at it, only to fall short once more. Again and again he tried, but in vain.Now he sat down and looked at the grapes in disgust.What a fool I am, he said. Here I am wearing myself out to get a bunch of sour grapes that are not worth gaping for.And off he walked very, very scornfully.There are many who pretend to despise and belittle that which is beyond their reach.",
+                "A journey of a thousand miles begins with a single step.",
+                "To be or not to be, that is the question.",
+                "All that glitters is not gold.",
+                "The only thing we have to fear is fear itself."
+            };
+
+            random = new Random();
+            labelTestText.Text = "";
+            textBoxInput.Enabled = false;
+            labelResults.Text = "";
+            stopwatch = new Stopwatch();
+
+            timer = new Timer();
+            timer.Interval = 1000; // 1 second
+            timer.Tick += Timer_Tick;1
+
+            buttonStart.Click += ButtonStart_Click;
+        }
+
+        private void ButtonStart_Click(object sender, EventArgs e)
+        {
+            currentTestText = testTexts[random.Next(testTexts.Count)];
+            labelTestText.Text = currentTestText;
+
+            textBoxInput.Clear();
+            textBoxInput.Enabled = true;
+            textBoxInput.Focus();
+
+            stopwatch.Reset();
+            stopwatch.Start();
+
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            int elapsedSeconds = (int)stopwatch.Elapsed.TotalSeconds;
+            labelTimer.Text = (timeLimit - elapsedSeconds).ToString() + "s";
+
+            if (elapsedSeconds >= timeLimit)
+            {
+                timer.Stop();
+                stopwatch.Stop();
+                textBoxInput.Enabled = false;
+                CalculateResults();
+            }
+        }
+
+        private void CalculateResults()
+        {
+            string userInput = textBoxInput.Text;
+            int wordsTyped = userInput.Split(new char[] { ' ', '\n' }, StringSplitOptions.RemoveEmptyEntries).Length;
+            double wpm = (wordsTyped / (double)timeLimit) * 60;
+
+            int correctChars = 0;
+            for (int i = 0; i < Math.Min(userInput.Length, currentTestText.Length); i++)
+            {
+                if (userInput[i] == currentTestText[i])
+                {
+                    correctChars++;
+                }
+            }
+            double accuracy = (double)correctChars / currentTestText.Length * 100;
+
+            labelResults.Text = String.Format("WPM: {0:F2}\nAccuracy: {1:F2}%", wpm, accuracy);
+        }
+    }
+}
